be28898202e743888d77505b27261dbfb2aca078
Barbara <barbara.blohorn@hotmail.fr>
2018-06-17 22:51:02 +0200

    Maquette_Responsive

---
+++ main.js
@@ -0,0 +1,10 @@
+1) bloc+element ont des identifiants unique (ne pas utiliser les id)
+2) on utilise pas les balise html dans le CSS mais les class
+3) Les cascades sont à éviter (ex: p .class)
+
+Ex:
+<div class="header">
+<img class="header__logo"/>
+<h1 class="titre"></h1>
+</div>
+
